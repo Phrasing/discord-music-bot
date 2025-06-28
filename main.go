e9fe684352eaa28f7106e2f796321e45759fd6ee
Phrasing <markmicciche@pm.me>
2025-06-28 15:13:24 -0400

    feat: Add automatic curl-cffi updater

--- main.go
+++ main.go
@@ -88,9 +88,18 @@ func startUpdateChecker() {
 		output, err := cmd.CombinedOutput()
 		if err != nil {
 			log.Printf("Error updating yt-dlp: %v\n%s", err, output)
+		} else {
+			log.Printf("yt-dlp update check complete:\n%s", output)
+		}
+
+		log.Println("Checking for curl-cffi updates...")
+		cmd = exec.Command("pip", "install", "curl_cffi", "--upgrade")
+		output, err = cmd.CombinedOutput()
+		if err != nil {
+			log.Printf("Error updating curl-cffi: %v\n%s", err, output)
 			return
 		}
-		log.Printf("yt-dlp update check complete:\n%s", output)
+		log.Printf("curl-cffi update check complete:\n%s", output)
 	}
 
 	go func() {
@@ -491,7 +500,7 @@ func playSound(s *discordgo.Session, guildID string, song *Song) {
 		"--get-url",
 		"-f", "bestaudio",
 		"--no-playlist",
-		"--impersonate", "chrome110",
+		"--impersonate", "chrome135",
 		videoURL,
 	}
 
